826d4157b24704df0a8ee1e7dda1815a6fa6ca50
Frank Wessels <fwessels@xs4all.nl>
2016-07-14 09:38:05 +0000

    Manipulate H

--- sha256block_arm64.go
+++ sha256block_arm64.go
@@ -2,6 +2,24 @@
 
 package sha256
 
+import "fmt"
+
+//go:noescape
+func block(h []uint32, p []uint8)
+
 func sha256() string {
+
+	h := []uint32{0x6a09e667,
+		0xbb67ae85,
+		0x3c6ef372,
+		0xa54ff53a,
+		0x510e527f,
+		0x9b05688c,
+		0x1f83d9ab,
+		0x5be0cd19}
+
+	fmt.Printf("%08x\n", h[0])
+	block(h[:], make([]uint8, 8, 8))
+	fmt.Printf("%08x\n", h[0])
 	return "hallo from arm64"
 }
--- sha256block_arm64.s
+++ sha256block_arm64.s
@@ -1,2 +1,8 @@
 // ARM version of SHA256
 
+TEXT	·block(SB), NOSPLIT, $0
+	MOVD   message+0(FP), R0
+	MOVD   0(R0), R1
+	ADD    $1, R1
+	MOVD   R1, 0(R0)
+	RET
