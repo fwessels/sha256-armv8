b94397f930f84a97a425bd6432f6d498cfe9f9df
Frank Wessels <fwessels@xs4all.nl>
2016-07-15 19:51:50 +0000

    Moved constants to assembly

--- sha256block_arm64.go
+++ sha256block_arm64.go
@@ -9,7 +9,7 @@ import (
 )
 
 //go:noescape
-func block(h []uint32, p, x2 []uint8, constants []uint32)
+func block(h []uint32, message []uint8)
 
 func Sha256() string {
 
@@ -22,23 +22,6 @@ func Sha256() string {
 		0x1f83d9ab,
 		0x5be0cd19}
 
-	constants := []uint32{0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
-		0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-		0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
-		0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-		0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-		0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
-		0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-		0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-		0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2}
-
 	dat, _ := ioutil.ReadFile("/home/linaro/gopath/src/github.com/minio/blake2b-simd/out.bin")
 
 	start := time.Now()
@@ -46,7 +29,7 @@ func Sha256() string {
 	blocks := len(dat) / 64
 
 	if blocks > 0 {
-		block(h[:], dat[:64*blocks], make([]uint8, 256, 256), constants[:])
+		block(h[:], dat[:64*blocks])
 	}
 	l := len(dat) - blocks*64
 
@@ -65,7 +48,7 @@ func Sha256() string {
 			buffer[i] = 0x0
 		}
 
-		block(h[:], buffer[:], make([]uint8, 256, 256), constants[:])
+		block(h[:], buffer[:])
 
 		// add (almost) one block of zero bytes
 		i = 0
@@ -79,7 +62,7 @@ func Sha256() string {
 		buffer[63-i] = uint8((len(dat) * 8) >> (uint32(i) * 8))
 	}
 
-	block(h[:], buffer[:], make([]uint8, 256, 256), constants[:])
+	block(h[:], buffer[:])
 
 	elapsed := time.Since(start)
 	fmt.Printf("SHA256 (size=%dM) took %s\n", len(dat)/1024/1024, elapsed)
